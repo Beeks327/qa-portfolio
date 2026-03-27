ce9629288b031be4bbf19fabb360e462ab52ae71
Beeks327 <167115784+Beeks327@users.noreply.github.com>
2026-03-27 14:30:03 +0000

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-document.getElementById("hamburgerBtn")?.addEventListener("click",()=>{document.getElementById("sidebar").classList.toggle("open")}); document.querySelectorAll(".accordion-btn").forEach(btn=>btn.addEventListener("click",()=>{const p=btn.nextElementSibling;p.style.display=p.style.display=="block"?"none":"block"}))
