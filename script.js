e63b21a75d8d7c73793cbaa13a18c613dab1d906
Beeks327 <167115784+Beeks327@users.noreply.github.com>
2026-03-27 13:32:32 +0000

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-document.getElementById('toggleTheme').onclick=()=>document.body.classList.toggle('dark');const acc=document.getElementsByClassName('accordion-btn');for(let i=0;i<acc.length;i++){acc[i].onclick=()=>{const panel=acc[i].nextElementSibling;panel.style.display=panel.style.display==='block'?'none':'block';};}
